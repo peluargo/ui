cb6c6880239c1eab0789fcefd764b7b522b226c0
Pedro L. A. Gonçalves <pedrolagoncalves@gmail.com>
2025-02-07 19:14:32 -0300

    refactor: from scratch

---
+++ src/pages/sign-up/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-btn>
+    Send verification code
+  </v-btn>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    title="Creating a new account"
+  >
+    <v-card-text>
+      <v-text-field
+        v-model="email"
+        label="Email"
+        placeholder="amazing.user@email.com"
+        hint="Please enter YOUR email address 👀"
+      />
+    </v-card-text>
+    
+    <v-card-actions class="d-flex flex-column">
+      <v-btn
+        block
+        color="primary"
+        variant="tonal"
+      >
+        Send verification code
+      </v-btn>
+              
+      <v-btn
+        block
+        color="primary"
+        variant="text"
+      >
+        I already have an account
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+const email = ref()
+</script>
